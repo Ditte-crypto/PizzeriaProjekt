eda97e5ff538179bfe27ae0ce5159f60e18adb16
Ditte-crypto <60691158+Ditte-crypto@users.noreply.github.com>
2020-03-30 09:57:20 +0200

    Ditte

--- src/Ordre.java
+++ src/Ordre.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class Ordre {
+    int id;
+    ArrayList<Pizza> pizzaer;
+    String status;
+
+    public void addPizza(Pizza) {
+
+    }
+
+    public double calculateTotal(Ordre) {
+        double total = 0.0;
+
+        return total;
+    }
+
+    public void setStatus(String str) {
+
+    }
 }
+
