de5b0c11d895163a4696e0e0c8494d8a3c666c56
Ditte-crypto <60691158+Ditte-crypto@users.noreply.github.com>
2020-04-01 09:41:21 +0200

    Ditte

--- src/Pizzeria.java
+++ src/Pizzeria.java
@@ -80,48 +80,6 @@ public class Pizzeria {
 
         }
     }
-
-}
-
-    public static void menuSettings() {
-        int choice = 0;
-
-        Scanner in = new Scanner(System.in);
-        while (choice != 6) {
-            choice = in.nextInt();
-            try {
-                if (choice == 1){
-                    showMenu();
-                    startNewOrder();
-                } else if (choice == 2) {
-                    listOrders();
-                    showInterface();
-                } else if (choice == 3) {
-                    //fjern eller tilføj pizza fra ordre
-                } else if (choice == 4) {
-                    //vis næste ordre
-                } else if (choice == 5) {
-                    //updateOrder
-                } else if (choice == 6) {
-                    showMenu();
-                }
-            } catch (IllegalArgumentException e) {
-                choice = 7;
-            }
-        }
-    }
-
-    public static void showInterface() {
-        System.out.println("******* PIZZERIA ALLA ALFONSO E MARIO *******\n"+
-                "1. Start ny ordre\n" +
-                "2. Vis aktive ordre\n" +
-                "3. Fjern eller tilføj Pizza fra ordre\n" +
-                "4. Vis næste ordre\n" +
-                "5. Opdater ordre status\n" +
-                "6. Vis menu\n" +
-                "7. Exit");
-    }
-
     public static void main(String[] args) {
         UI.showInterface();
         UI.menuSettings();
